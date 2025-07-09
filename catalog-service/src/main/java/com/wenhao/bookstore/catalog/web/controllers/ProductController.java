c862eeeb8b95e1ff2f38e3119622f67294c9eec8
WenhaoYou1 <ericwyou1@gmail.com>
2025-07-08 22:56:45 -0400

    apply spotless and add test-data.sql

--- catalog-service/src/main/java/com/wenhao/bookstore/catalog/web/controllers/ProductController.java
+++ catalog-service/src/main/java/com/wenhao/bookstore/catalog/web/controllers/ProductController.java
@@ -1,16 +1,15 @@
 package com.wenhao.bookstore.catalog.web.controllers;
 
+import com.wenhao.bookstore.catalog.domain.PagedResult;
+import com.wenhao.bookstore.catalog.domain.Product;
+import com.wenhao.bookstore.catalog.domain.ProductNotFoundException;
+import com.wenhao.bookstore.catalog.domain.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.wenhao.bookstore.catalog.domain.PagedResult;
-import com.wenhao.bookstore.catalog.domain.Product;
-import com.wenhao.bookstore.catalog.domain.ProductNotFoundException;
-import com.wenhao.bookstore.catalog.domain.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
--- catalog-service/src/test/java/com/wenhao/bookstore/catalog/domain/ProductRepositoryTest.java
+++ catalog-service/src/test/java/com/wenhao/bookstore/catalog/domain/ProductRepositoryTest.java
@@ -1,9 +1,9 @@
 package com.wenhao.bookstore.catalog.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.math.BigDecimal;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
