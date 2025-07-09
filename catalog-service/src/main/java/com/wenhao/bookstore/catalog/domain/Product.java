7f1e228770d0946142e81f7d149b2b433c892941
WenhaoYou1 <ericwyou1@gmail.com>
2025-07-08 22:49:28 -0400

    update catalog-service api

---
+++ catalog-service/src/main/java/com/wenhao/bookstore/catalog/domain/Product.java
@@ -0,0 +1,5 @@
+package com.wenhao.bookstore.catalog.domain;
+
+import java.math.BigDecimal;
+
+public record Product(String code, String name, String description, String imageUrl, BigDecimal price) {}
---
+++ catalog-service/src/main/java/com/wenhao/bookstore/catalog/web/controllers/ProductController.java
@@ -0,0 +1,36 @@
+package com.wenhao.bookstore.catalog.web.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wenhao.bookstore.catalog.domain.PagedResult;
+import com.wenhao.bookstore.catalog.domain.Product;
+import com.wenhao.bookstore.catalog.domain.ProductNotFoundException;
+import com.wenhao.bookstore.catalog.domain.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+class ProductController {
+    private final ProductService productService;
+
+    ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
+        return productService.getProducts(pageNo);
+    }
+
+    @GetMapping("/{code}")
+    ResponseEntity<Product> getProductByCode(@PathVariable String code) {
+        return productService
+                .getProductByCode(code)
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> ProductNotFoundException.forCode(code));
+    }
+}
