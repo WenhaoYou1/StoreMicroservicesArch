49f95165254f7d82bc7b6c8fc2bbbb3704f7f006
WenhaoYou1 <ericwyou1@gmail.com>
2025-07-14 17:22:57 -0400

    add comments

--- catalog-service/src/main/java/com/wenhao/bookstore/catalog/web/controllers/ProductController.java
+++ catalog-service/src/main/java/com/wenhao/bookstore/catalog/web/controllers/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // 接口方法返回对象, 转换成JSON文本
 @RequestMapping("/api/products")
 class ProductController {
     private final ProductService productService;
@@ -19,8 +19,11 @@ class ProductController {
     ProductController(ProductService productService) {
         this.productService = productService;
     }
-
-    @GetMapping
+    // @PostMapping 增加
+    // @PutMapping 修改
+    // @DeleteMapping 删除
+    // 注意: 不存在@Get的写法, 不过可以写 @RequestMapping(method = RequestMethod.GET)
+    @GetMapping // 表示查询
     PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
         return productService.getProducts(pageNo);
     }
